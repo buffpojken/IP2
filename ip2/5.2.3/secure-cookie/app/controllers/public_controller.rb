2d8b63742abed1f8f1899e3e7bc99734b6f14d07
buffpojken <daniel@sykewarrior.com>
2011-07-13 02:00:32 +0200

    added secure cookies

---
+++ ip2/5.2.3/secure-cookie/app/controllers/public_controller.rb
@@ -0,0 +1,20 @@
+##### Author: Daniel Sundström  #####
+##### Date: July 13, 2011 - Jul:07:42#####
+##### License: MIT License    #####
+class PublicController < ApplicationController
+
+  def index
+    cookies[:value1] = {
+      :value => "ninja", 
+      :expires => 3.hours.from_now, 
+      :secure => true
+    }
+
+    cookies[:value2] = {
+      :value => "samurai", 
+      :expires => 3.hours.from_now, 
+      :secure => true
+    }
+  end
+  
+end
---
+++ ip2/5.2.3/secure-cookie/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecureCookie::Application.config.secret_token = 'df910872fe38d397310e00245184fab5848a3d711986bd033e971ef4c2d77f9be8ee86094106a0ec27b167a61da17b094e3ef77473e3838d555f4c2e127e2f51'
