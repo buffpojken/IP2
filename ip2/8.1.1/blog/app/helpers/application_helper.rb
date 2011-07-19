afc38f04b134db68fb3c79d9dfa7b4ee9c89fee1
buffpojken <daniel@sykewarrior.com>
2011-07-20 01:46:14 +0200

    now language complete

--- ip2/8.1.1/blog/app/helpers/application_helper.rb
+++ ip2/8.1.1/blog/app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
 
   def transform(txt = "")
-    txt.gsub(/\*(.+)\*/){|m|"<i>#{$1}</i>"}.gsub(/\-(.+)\-/){|m|"<u>#{$1}</u>"}
+    txt.gsub(/\*(.+)\*/){|m|"<i>#{$1}</i>"}
+    .gsub(/\-(.+)\-/){|m|"<u>#{$1}</u>"}
+    .gsub(/\[(.+)\]/){|m|"<a href='/publics/#{$1}'>#{$1}</a>"}
+    .gsub(/\#(.+)\#/){|m|"<h2>#{$1}</h2>"}
   end
 
 end
