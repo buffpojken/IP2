54420c3ae860f1472c4d9f3be61c7ceb00bcd5c0
buffpojken <daniel@sykewarrior.com>
2011-07-19 23:58:02 +0200

    added initial

---
+++ ip2/6.1.2/send-file-email/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendFileEmail::Application.config.secret_token = '2a526a81c37344e1271270e48ec601727a2d9a38a7ee4dc5dadc1e9a8d22c2f89c90d4b7c03032a44a3a961cf6c8a943e04026e67eca25559d40128c130c9439'
